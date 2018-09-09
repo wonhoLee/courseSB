e5f4c82b152401ff9174fc066b48466ebca4ba76
wonhoLee <lwonho@hotmail.com>
2018-09-09 16:07:08 +0900

    init

---
+++ src/main/java/net/course/CourseSbApplication.java
@@ -0,0 +1,12 @@
+package net.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSbApplication.class, args);
+	}
+}
