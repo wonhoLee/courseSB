32f1b85bfde22f4e655bdc17f1dde4a6a69f21de
wonhoLee <lwonho@hotmail.com>
2018-09-11 15:39:04 +0900

    html 중복값 제거, form, list 작업

--- src/main/java/net/course/web/UserController.java
+++ src/main/java/net/course/web/UserController.java
@@ -1,30 +1,37 @@
 package net.course.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.course.domain.User;
+import net.course.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-	private List<User> users = new ArrayList<>();
 	
-	@PostMapping("/create")
+	@Autowired
+	private UserRepository userRepository;
+	
+	@PostMapping("")
 	public String create(User user) {
-		System.out.println("inputValue : " + user.toString());
-		users.add(user);
-		
-		return "redirect:/list";
+		System.out.println("inputValue : " + user);
+		userRepository.save(user);
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/list")
-	public String list(Model model) {
-		model.addAttribute("users", users);
-		return "list";
+	@GetMapping("")
+	public String userlist(Model model) {
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list";
+	}
+	
+	@GetMapping("/form")
+	public String userForm() {
+		return "/user/form";
 	}
 }
