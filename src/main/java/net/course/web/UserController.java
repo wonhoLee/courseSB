dd650a794b5adefdec2ada4feaaf4250565e24d7
wonhoLee <lwonho@hotmail.com>
2018-09-11 09:51:06 +0900

    h2 database setting - ing

--- src/main/java/net/course/web/UserController.java
+++ src/main/java/net/course/web/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import net.course.domain.User;
+
 @Controller
 public class UserController {
 	private List<User> users = new ArrayList<>();
