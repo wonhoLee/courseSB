d3b7cdbcefcd230fecca385f29307a295ab797ba
wonhoLee <lwonho@hotmail.com>
2018-09-12 10:04:04 +0900

    중복( footer) 관련 정리 로그인 기능구현 완료

--- src/main/java/net/course/domain/UserRepository.java
+++ src/main/java/net/course/domain/UserRepository.java
@@ -3,5 +3,6 @@ package net.course.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+	User findByUserId(String userId);
 
 }
--- src/main/java/net/course/web/UserController.java
+++ src/main/java/net/course/web/UserController.java
@@ -1,5 +1,7 @@
 package net.course.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,6 +20,29 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "/user/login";
+	}
+	
+	@PostMapping("/login")
+	public String login(String userId, String password, HttpSession session) {
+		User user = userRepository.findByUserId(userId);
+		if(user == null) {
+			System.out.println("Login Failure! - userId");
+			return "redirect:/users/loginForm";
+		}
+		
+		if(!password.equals(user.getPassword())) {
+			System.out.println("Login Failure! - password");
+			return "redirect:/users/loginForm";
+		}
+		System.out.println("Login Success!");
+		session.setAttribute("user", user);
+		
+		return "redirect:/";
+	}
 	
 	@PostMapping("")
 	public String create(User user) {
