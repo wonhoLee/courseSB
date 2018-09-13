83e1cc651bd34ec6e0882911457666f1367fe2f0
wonho <lwonho@hotmail.com>
2018-09-13 22:10:03 +0900

    질문상세보기

--- src/main/java/net/course/web/QuestionController.java
+++ src/main/java/net/course/web/QuestionController.java
@@ -1,12 +1,12 @@
 package net.course.web;
 
-import java.time.LocalDateTime;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -39,6 +39,12 @@ public class QuestionController {
 		questioinRepository.save(newQuestion);
 		
 		return "redirect:/";
+	}
+	
+	@GetMapping("/{id}")
+	public String show(@PathVariable Long id, Model model) {
+		model.addAttribute("questions", questioinRepository.findById(id).get());
+		return "/qna/show";
 	}
 	
 }
