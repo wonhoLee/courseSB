291897f4b9e53c8d310216b8d1cac60e98d8457c
wonho <lwonho@hotmail.com>
2018-09-14 13:55:21 +0900

    go.sh modify

--- go.sh
+++ go.sh
@@ -3,5 +3,6 @@ git pull
 ~/apache-tomcat-8.5.34/bin/shutdown.sh
 ./mvnw clean package
 cd target
-mv courseSB-1.0 ~/apache-tomcat-8.5.34/webapps/ROOT
+rm -rf ~/apache-tomcat-8.5.34/webapps/ROOT/
+mv courseSB-1.0/ ~/apache-tomcat-8.5.34/webapps/ROOT/
 ~/apache-tomcat-8.5.34/bin/startup.sh
