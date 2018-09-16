204a0d6a1ba6ac902c9b14d24a719cfd5bb7acfd
wonho <lwonho@hotmail.com>
2018-09-16 18:18:12 +0900

    서버용 쉘 수정

--- go.sh
+++ go.sh
@@ -6,3 +6,4 @@ cd target
 rm -rf ~/apache-tomcat-8.5.34/webapps/ROOT/
 mv courseSB-1.0/ ~/apache-tomcat-8.5.34/webapps/ROOT/
 ~/apache-tomcat-8.5.34/bin/startup.sh
+tail -500f ~/apache-tomcat-8.5.34/logs/catalina.out
